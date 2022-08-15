a00f2eecd6f11a18180f6ae14cd34fc4cbb67e7f
Nico <nico@bieberich.info>
2022-08-15 19:08:31 +0200

    First steps

--- DIC/Debug/makedep.mk
+++ DIC/Debug/makedep.mk
@@ -2,5 +2,17 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+canlib.c
+
+CAN_Data.c
+
+display_data.c
+
+display_functions.c
+
+error.c
+
 main.c
+
+system_config.c
 
